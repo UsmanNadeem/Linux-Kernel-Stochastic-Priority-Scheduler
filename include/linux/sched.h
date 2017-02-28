6bd975fe8b13a51a3b79ea07798ddcc2866ee036
UsmanNadeem <usmanone@gmail.com>
2017-02-28 16:04:30 -0500

    misc stuff

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -38,6 +38,11 @@
 #define SCHED_BATCH		3
 /* SCHED_ISO: reserved but not implemented yet */
 #define SCHED_IDLE		5
+	
+#ifdef CONFIG_SCHED_NEW_POLICY
+#define SCHED_NEW		6
+#endif
+
 /* Can be ORed in to make sure the process is reverted back to SCHED_NORMAL on fork */
 #define SCHED_RESET_ON_FORK     0x40000000
 
@@ -1222,6 +1227,10 @@ struct sched_rt_entity {
 struct rcu_node;
 
 struct task_struct {
+	#ifdef CONFIG_SCHED_NEW_POLICY
+		unsigned long long numTickets;
+	#endif
+
 	volatile long state;	/* -1 unrunnable, 0 runnable, >0 stopped */
 	void *stack;
 	atomic_t usage;
